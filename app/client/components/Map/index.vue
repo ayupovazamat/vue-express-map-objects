bef0ac938bc249317c8f9ab03f8eaf766535e4d3
ayupovazamat <ayupovazamat@gmail.com>
2021-10-11 20:30:53 +0500

    add promise function addMarkers()

--- app/client/components/Map/index.vue
+++ app/client/components/Map/index.vue
@@ -64,13 +64,9 @@
         this.icons['activeMarker'] = L.icon({iconUrl: "/images/icons/marker-active.svg"});
         this.icons.defaultMarker = L.icon({iconUrl: "/images/icons/marker-default.svg"});
 
-
         // добавляем маркеры на карту
-        // todo add Promise https://codesandbox.io/s/beautiful-danilo-8xmym?file=/src/index.js
-        // добавление маркеров сделать промисом
-        setTimeout(()=>{
-          this.addMarkers();
-        }, 1000)
+        await new Promise(() => this.addMarkers());
+
       }
     },
     beforeDestroy() {
@@ -81,23 +77,21 @@
     methods: {
       ...mapActions([MAIN__GET_SELECT_OBJECT]),
       async initializationMap() {
-        this.map = L.map("mapContainer").setView([6.1405547, 43.85742], 3);
+        this.map = L.map("mapContainer");
         L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
           attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         }).addTo(this.map);
 
-        // центриуем карту по объектам
+        // центрируем карту по объектам
         this.map.fitBounds(this.bounds);
 
         // создаем группу
         this.group = L.layerGroup();
       },
       async addMarkers() {
-        let icon = L.icon({iconUrl: "/images/icons/marker-default.svg"});
-
-        this.markers = await this.objects.map((el, i) => {
+        this.markers = await this.objects.map((el) => {
           //  создаем маркер
-          let marker = new L.marker([el.latitude, el.longitude],{id: el.id}).addTo(this.map).on('click', (e)=>{
+          let marker = new L.marker([el.latitude, el.longitude],{id: el.id}).addTo(this.map).on('click', ()=>{
             // добавляем событие клика
             this.selectObject(el.id);
           })
